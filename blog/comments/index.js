57169e4ef6980142f35e3e2183ae97c3c9a915b2
Bhupendra Tiwari <bhupentiwari@live.com>
2023-07-10 23:36:54 +0530

    upto 47 lecture

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -15,15 +15,18 @@ app.post('/posts/:id/comments',async (req,res)=>{
     const commentId = randomBytes(6).toString('hex');
     const {content} = req.body;
     const comments = commentsByPostId[req.params.id] || [];
-    comments.push({id : commentId, content});
-    commentsByPostId[req.params.id] = comments;
 
+    comments.push({id : commentId, content,status: 'pending'});
+
+    commentsByPostId[req.params.id] = comments;
+    console.log('CommentCreated')
     await axios.post('http://localhost:4005/events', {
         type : 'CommentCreated',
         data : {
          id:commentId,
          content,
-         postId : req.params.id
+         postId : req.params.id,
+         status : 'pending'
         }
       }).catch((error) => {
         console.log(error.message);
@@ -32,7 +35,33 @@ app.post('/posts/:id/comments',async (req,res)=>{
     res.status(200).send(comments);
 });
 
-app.post('/events',(req,res) => {
+app.post('/events',async (req,res) => {
+
+    const {type,data} = req.body;
+    if(type === 'CommentModerated'){
+        console.log('CommentModerated')
+        const {postId,id,status,content} = data;
+        console.log(data)
+       
+        const comments = commentsByPostId[postId];
+        const comment  = comments.find(c => {
+            return c.id == id;
+        })
+        comment.status = status;
+
+    await axios.post('http://localhost:4005', {
+        type : 'CommentUpdated',
+        data : {
+            id,
+            status:status,
+            postId,
+            content
+        }
+    }).catch((error) => {
+        console.log(error.message);
+    });
+
+    }
     console.log('Received Event', req.body.type);
     res.send({});
 });
